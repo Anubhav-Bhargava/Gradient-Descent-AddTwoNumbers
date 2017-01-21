1ecd3384cc8093a2e01a0d9233509560c52ef109
Anubhav-Bhargava <anubhavbhargava007@gmail.com>
2017-01-21 12:40:08 +0530

    Update Gradient-Descent.py

--- Gradient-Descent.py
+++ Gradient-Descent.py
@@ -3,10 +3,14 @@ import numpy as np
 x=np.array([[3,6],[4,7],[2,6],[4,8]])
 y=[9,11,8,12]
 
+np.random.seed(2)
+
 m,n=np.shape(x)
 theta=np.random.rand(n)
+
 print "Initial value of weights" 
 print theta
+
 xTrans = x.transpose()
 for i in range(0, 100000):
         hypothesis = np.dot(x, theta)
@@ -14,8 +18,7 @@ for i in range(0, 100000):
         gradient = np.dot(xTrans, loss) / m
         theta = theta - 0.0005 * gradient
 
-
-
 print "Final value of weights"
 print theta
-print np.dot(np.array([[3,6]]),theta)
+print "Addition of 34 and 6 is equal to"
+print np.dot(np.array([[34,6]]),theta)
