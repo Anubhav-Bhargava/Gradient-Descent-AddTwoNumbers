f0fa7614f805b19575f44ecb9b709e2c66c4f95f
Anubhav-Bhargava <anubhavbhargava007@gmail.com>
2017-01-21 12:12:34 +0530

    commited

---
+++ Gradient-Descent.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x=np.array([[3,6],[4,7],[2,6],[4,8]])
+y=[9,11,8,12]
+
+m,n=np.shape(x)
+theta=np.random.rand(2)
+print "Initial value of weights" 
+print theta
+xTrans = x.transpose()
+for i in range(0, 100000):
+        hypothesis = np.dot(x, theta)
+        loss = hypothesis - y
+        gradient = np.dot(xTrans, loss) / m
+        theta = theta - 0.0005 * gradient
+
+
+
+print "Final value of weights"
+print theta
+print np.dot(np.array([[3,6]]),theta)
